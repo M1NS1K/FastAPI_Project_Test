ad822a60be1f120cd77c83d8895f6a9bf0699130
M1NS1K <rlaalstlr2001@gmail.com>
2024-03-25 14:53:34 +0900

    change library

--- socket/server.py
+++ socket/server.py
@@ -1,46 +1,81 @@
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
-import socketio
-import uvicorn
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 
-# Socket.IO 서버 인스턴스 생성
-# CORS 문제를 방지하기 위해 모든 출처를 허용합니다. (운영 환경에서는 보안을 위해 이 설정을 제한하는 것이 좋습니다.)
-# async_mode를 'asgi'로 설정하여 비동기 ASGI 애플리케이션과 호환되도록 합니다.
-sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='asgi')
-
-# FastAPI 인스턴스 생성
 app = FastAPI()
 
-# Socket.IO 서버를 ASGI 애플리케이션으로 변환
-socket_app = socketio.ASGIApp(sio)
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
-# FastAPI 애플리케이션에 Socket.IO 서버를 마운트
-# 이를 통해 FastAPI 경로는 일반 HTTP 요청을 처리하고, Socket.IO 경로는 웹소켓 연결을 처리할 수 있습니다.
-app.mount("/", socket_app)
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def get():
+    return HTMLResponse(html)
 
-@app.get("/favicon.ico", response_class=PlainTextResponse)
-def favicon():
-    return ""
 
-# 연결 이벤트 핸들러
-@sio.on("connect")
-async def connect(sid, environ):
-    print("Client connected", sid)
-
-# 메시지 수신 이벤트 핸들러
-@sio.on("message")
-async def my_message(sid, data):
-    print("Message from client:", data)
-    await sio.emit('my_response', {'data': 'This is a response from server!'})
-
-# 연결 해제 이벤트 핸들러
-@sio.on("disconnect")
-async def disconnect(sid):
-    print("Client disconnected", sid)
-    
-if __name__=="__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=7777, lifespan="on", reload=True)
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
